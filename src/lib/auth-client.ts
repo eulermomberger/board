72c0df067bbac89ba1eeb6582cffba2010ca9c1b
euler <mombergereuler@gmail.com>
2026-09-23 21:34:27 -0300

    ✨feat: Autenticação com Better Auth

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,7 @@
+import { createAuthClient } from "better-auth/react";
+
+import { clientEnv } from "@/env";
+
+export const authClient = createAuthClient({
+  baseURL: clientEnv.NEXT_PUBLIC_API_URL,
+});
